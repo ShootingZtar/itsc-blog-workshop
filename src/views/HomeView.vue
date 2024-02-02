df21dc9d6ed8d9350d2d939145017d5512127f61
nat.lee <odds-kanisorn.lee@tech.jago.com>
2024-02-03 01:34:59 +0700

    fix design HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,19 +32,21 @@ const { result: blogsResult } = useQuery<BlogsQuery>(gql`
   </div>
   <div>
     <div v-for="blog in blogsResult?.blogs" :key="blog.blogId" class="card mb-8">
-      <div class="w-full rounded-t-lg bg-[#FF9E22] p-4 font-bold">{{ blog.title }}</div>
-      <div class="grid grid-cols-2 gap-4 w-full rounded-b-lg bg-[#FFDDB0] p-4">
-        <div :style="`background-image: url(${blog.thumbnailUrl});`" class="rounded-3xl bg-center h-40 sm:h-56 md:h-72">
-        </div>
+      <div class="w-full rounded-t-3xl bg-[#FF9E22] p-4 font-bold">{{ blog.title }}</div>
+      <div class="grid grid-cols-2 gap-4 w-full rounded-b-3xl bg-[#FFDDB0] p-4">
+        <div :style="`background-image: url(${blog.thumbnailUrl});`"
+          class="rounded-3xl bg-cover bg-center h-40 sm:h-56 md:h-72"></div>
         <div class="flex flex-col">
           <div class="grow">{{ blog.description }}</div>
           <div class="flex justify-end">
             <button class="bg-[#4527A0] text-white rounded-lg py-4 ml-4 w-[160px]">
               <span>Edit</span>
             </button>
-            <button class="bg-[#2962FF] text-white rounded-lg py-4 ml-4 w-[160px]">
-              <span>Read More</span>
-            </button>
+            <RouterLink :to="{ name: 'blog-detail', params: { blogId: blog.blogId } }">
+              <button class="bg-[#2962FF] text-white rounded-lg py-4 ml-4 w-[160px]">
+                <span>Read More</span>
+              </button>
+            </RouterLink>
           </div>
         </div>
       </div>
